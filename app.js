c93e7462943b1b00135dd55b9abd8c34d3ed108b
Hsin-Chieh <u10116004@go.utaipei.edu.tw>
2015-12-17 03:27:52 -0500

    basic

--- app.js
+++ app.js
@@ -1,36 +1,74 @@
+require('./db'); // connct to our DB
 
-/**
- * Module dependencies.
- */
+var express = require('express');
+var http = require('http');
+var path = require('path');
+var mongoose = require('mongoose');
+var passport = require('passport');
+var partials = require('express-partials');
+var routes  = require( './routes' );
+var user  = require( './routes/user' );
+var item  = require( './routes/item' );
+// var flash = require('connect-flash');
 
-var express = require('express')
-  , routes = require('./routes');
+// require('./config/passport'); // TODO [FB] : Passport configuration
 
-var app = module.exports = express.createServer();
+var app = express();
+// var Vote = mongoose.model('Vote'); // TODO [DB] : Get Vote model
 
-// Configuration
+// all environments
+app.set('port', process.env.PORT || 3000);
+app.set('views', path.join(__dirname, '/public/html'));
+app.set('view engine', 'ejs');
+app.use(partials());
+app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.json());
+app.use(express.bodyParser());
+app.use(express.urlencoded());
+app.use(express.methodOverride());
 
-app.configure(function(){
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'jade');
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(app.router);
-  app.use(express.static(__dirname + '/public'));
-});
+app.use(express.cookieParser("heyheyhey u can't guess my cookie key!"));
+app.use(express.cookieSession());
 
-app.configure('development', function(){
-  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
-});
+// https://github.com/jaredhanson/passport#middleware
+// app.use(passport.initialize());
+// app.use(passport.session());
+// // Session based flash messages
+// app.use(flash());
 
-app.configure('production', function(){
+app.use(app.router);
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+// development only
+if ('development' == app.get('env')) {
   app.use(express.errorHandler());
-});
+}
+
 
 // Routes
-
 app.get('/', routes.index);
+app.get('/home',routes.home);
+app.get('/supply/:id',routes.supply);
+app.get('/need/:id',routes.need);
+app.post('/create',routes.create);
+app.get('/upload', routes.upload);
+app.post('/uploadsupply', routes.uploadsupply);
+app.post('/uploadneed', routes.uploadneed);
+app.post('/login', routes.login);
+app.get('/item/:charity/:id',item.item);
+app.post('/deal',item.deal);
+app.get( '/destroy/:id', item.destroy );
 
-app.listen(3000, function(){
-  console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
+app.get('/user', user.index);
+app.post('/user', user.create);
+app.post('/user/login', user.login);
+app.get('/user/logout', user.logout);
+app.get('/profile/:id', user.profile);
+
+app.post('/donate', user.donate);
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
 });
